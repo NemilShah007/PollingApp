5daf45e6e13612f1681f8e33036fbe17f3a2afc5
NemilShah007 <shahnemil103@gmail.com>
2020-02-08 23:07:58 +0530

    Initial backend created

---
+++ app.js
@@ -0,0 +1,26 @@
+//jshint esversion:6
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+const poll = require("./routes/poll");
+
+//Set public folder
+app.use(express.static(path.join(__dirname,"public")));
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Enable CORS
+app.use(cors());
+
+app.use("/poll",poll);
+
+
+//Start Server
+app.listen(3000,() => console.log("Server started on port 3000"));
---
+++ routes/poll.js
@@ -0,0 +1,27 @@
+//jshint esversion:6
+
+const express = require('express');
+const router = express.Router();
+const Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: '944579',
+  key: '22606a69db895ee7e54d',
+  secret: 'bfa43df0c91e969ff864',
+  cluster: 'ap2',
+  encrypted: true
+});
+
+router.get("/",(req,res) => {
+  res.send("Poll");
+});
+
+router.post("/",(req,res)=>{
+  pusher.trigger('os-poll', 'os-vote', {
+    points: 1,
+    os: req.body.os
+  });
+  return res.json({success:true,message:"Thank you for voting"});
+});
+
+module.exports = router;
